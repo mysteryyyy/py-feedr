cd4b34e20d55da303d3bba4cce0270c144a293a2
shunyi <M157q.tw@gmail.com>
2015-12-24 04:48:30 +0800

    Use localtime for localtime_log

--- feedr/monitor.py
+++ feedr/monitor.py
@@ -50,7 +50,7 @@ class MonitorFeedUpdate(object):
 
         unchecked_hash = (self.rss_latest_sha256(),)
         check = self.dbmanager.check_for_existing_update(unchecked_hash)
-        localtime_log = time.strftime("%d %b %Y - %H:%M:%S", time.gmtime())
+        localtime_log = time.strftime("%d %b %Y - %H:%M:%S", time.localtime())
 
         if check:
             # FIXME: Use logging module
@@ -69,8 +69,7 @@ class MonitorFeedUpdate(object):
                     format(self.feed_name, localtime_log, entry_table_hash))
             self.dbmanager.create_latest_rss_entry(
                 self.latest_rss_entry_to_db())
-            self.tweetupdate.tweet_latest_update(self.feed_name,
-                                                 self.latest_entry)
+            self.tweetupdate.tweet_latest_update(self.latest_entry)
             print('[{0}] - {1} - New update posted: {2}\n'
                   '[{0}] - {1} - Update title: {3}\n'
                   '[{0}] - {1} - Published: {4}\n'.format(
@@ -88,13 +87,14 @@ class MonitorFeedUpdate(object):
 
         cur_title = self.latest_entry['title']
         last_table_entry = self.dbmanager.get_last_table_entry()
-        if not last_table_entry: # empty table
+        if not last_table_entry:  # empty table
             return False
         else:
             prev_title = last_table_entry[3]
 
         if prev_title in cur_title:
             return True
+
         delta = difflib.SequenceMatcher(None, cur_title, prev_title)
         if delta.ratio() >= 0.75:
             return True
@@ -110,8 +110,11 @@ class MonitorFeedUpdate(object):
         '''
         entry = self.latest_entry
         genhash = hashlib.sha256()
-        genhash.update((entry['published'] + entry['title']
-                        + entry['link']).encode('utf-8'))
+        genhash.update(
+            (
+                entry['published'] + entry['title'] + entry['link']
+            ).encode('utf-8')
+        )
         return genhash.hexdigest()
 
     def latest_rss_entry_to_db(self):
@@ -125,4 +128,3 @@ class MonitorFeedUpdate(object):
                   entry['link'])
 
         return update
-
