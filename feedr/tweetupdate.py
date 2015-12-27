312b6f93cafae38dca364d78f63133cd878b9429
shunyi <M157q.tw@gmail.com>
2015-12-27 12:18:01 +0800

    Refactor tweet_latest_update()

--- feedr/tweetupdate.py
+++ feedr/tweetupdate.py
@@ -1,7 +1,9 @@
 import urllib.error
 import urllib.parse
 import urllib.request
+import traceback
 from collections import OrderedDict
+from pprint import pprint
 
 from bs4 import BeautifulSoup
 from html2text import html2text
@@ -24,6 +26,12 @@ class TweetUpdate(object):
         # Twitter object
         self.twitter_api = Twitter(auth=OAuth(oauth_key, oauth_secret,
                                               consumer_key, consumer_secret))
+        self.msg = OrderedDict((
+            ('title', ''),
+            ('url', ''),
+            ('summary', ''),
+            ('img_url', ''),
+        ))
 
     def delete_last_tweet(self):
         '''
@@ -45,108 +53,127 @@ class TweetUpdate(object):
         elif hasattr(feed_entry, 'description'):
             entry_html = feed_entry.description
         else:
-            entry_html = None
+            return None
 
-        if entry_html:
-            soup = BeautifulSoup(entry_html, 'html.parser')
-            img_tag = soup.find('img')
+        soup = BeautifulSoup(entry_html, 'html.parser')
+        img_tag = soup.find('img')
+        try:
+            img_url = img_tag['src']
+        except KeyError:
+            pprint(img_tag)
+            pprint("This <img> tag has no <src>")
+            return None
+        except TypeError:   # entry_html doesn't have <img> tag
+            return None
+        else:
+            img_url_splited = urllib.parse.urlsplit(img_url)
+            img_url = img_url_splited._replace(
+                netloc=urllib.parse.quote(img_url_splited.netloc),
+                path=urllib.parse.quote(img_url_splited.path),
+            ).geturl()
+            return img_url
 
-            try:
-                return img_tag['src']
-            except KeyError:
-                print(img_tag)
-                print("This <img> tag has no <src>")
-                return None
-            except TypeError:   # entry_html doesn't have <img> tag
-                return None
+    def msg_to_string(self):
+        return '\n'.join(filter(bool, self.msg.values()))
 
-    def tweet_latest_update(self, feed_entry):
-        '''
-        Tweets the latest update, logs when doing so.
-        '''
+    def msg_length(self):
+        return len(self.msg_to_string())
 
-        msg_limit_length = 140
+    def get_msg_limit_length_and_set_urls(self, feed_entry):
         TWEET_URL_LENGTH = 24
         TWEET_IMG_LENGTH = 25
-
-        url = feed_entry['link']
-        if url:
-            msg_limit_length -= TWEET_URL_LENGTH
-
-        img_url = self.get_entry_img_url(feed_entry)
-        if img_url:
-            msg_limit_length -= TWEET_IMG_LENGTH
-
-        msg = OrderedDict((
-            ('title', None),
-            ('url', None),
-            ('summary', None),
-            ('img_url', None),
-        ))
-
-        def msg_length():
-            return len('\n'.join(filter(bool, msg.values())))
-
-        msg['title'] = feed_entry['title'].strip()
-        if msg_length() - msg_limit_length > 0:
-            msg['title'] = '{}{}'.format(
-                msg['title'][:msg_limit_length - 3],
-                '.'*3,
-            )
-
-        elif 'summary' in feed_entry:
-            msg['summary'] = html2text(feed_entry['summary']).strip()
-
-            if msg_length() - msg_limit_length > 0:
-                summary_length = msg_limit_length - msg_length()
-
-                stripped_summary = '{}{}'.format(
-                    msg['summary'][:summary_length - 3],
-                    '.'*3,
-                )
-                msg['summary'] = stripped_summary
-
-        print(msg_length())
-        print('\n'.join(filter(bool, msg.values())))
-        msg['url'] = url
-
-        img_url_splited = urllib.parse.urlsplit(img_url)
-        img_url = img_url_splited._replace(
-            netloc=urllib.parse.quote(img_url_splited.netloc),
-            path=urllib.parse.quote(img_url_splited.path),
-        ).geturl()
+        msg_limit_length = 140
 
         try:
-            tempfile, headers = urllib.request.urlretrieve(img_url)
+            url = feed_entry['link']
+        except:
+            pass
+        else:
+            msg_limit_length -= TWEET_URL_LENGTH
+            self.msg['url'] = url
+
+        try:
+            img_url = self.get_entry_img_url(feed_entry)
+        except:
+            pass
+        else:
+            msg_limit_length -= TWEET_IMG_LENGTH
+            self.msg['img_url'] = img_url
+
+        return msg_limit_length
+
+    def cram_the_msg(self, feed_entry, msg_limit_length):
+        try:
+            self.msg['title'] = feed_entry['title'].strip()
+        except:
+            pass
+        if self.msg_length() > msg_limit_length:
+            self.msg['title'] = self.msg['title'][:msg_limit_length]
+
+        try:
+            self.msg['summary'] = html2text(feed_entry['summary']).strip()
+        except:
+            pass
+        if self.msg_length() > msg_limit_length:
+            trimmed_summary_index = msg_limit_length - self.msg_length()
+            self.msg['summary'] = self.msg['summary'][:trimmed_summary_index]
+
+        pprint(self.msg_length())
+        pprint(self.msg)
+
+    def tweet_with_no_media(self):
+        try:
+            return self.twitter_api.statuses.update(
+                status=self.msg_to_string()
+            )
+        except api.TwitterHTTPError:
+            pprint("Maybe message is too long, remove summary")
+            self.msg.pop('summary')
+            pprint(self.msg_length())
+            pprint(self.msg)
+            return self.twitter_api.statuses.update(
+                status=self.msg_to_string()
+            )
+        except:
+            traceback.print_exc()
+
+    def tweet_with_media(self):
+        try:
+            tempfile, headers = urllib.request.urlretrieve(self.msg['img_url'])
         except TypeError:
-            pass    # img_url is None
+            pprint('img_url is None, tweet with media url.')
+            raise
         except urllib.error.URLError:
-            print('Error while urlretrieving media, tweet with media url.')
-            msg['img_url'] = img_url
+            pprint('Error while urlretrieving media, tweet with media url.')
+            raise
         else:
             with open(tempfile, 'rb') as imgfile:
                 img = imgfile.read()
 
             urllib.request.urlcleanup()
             params = {
-                'status': '\n'.join(filter(bool, msg.values())),
+                'status': self.msg_to_string(),
                 'media[]': img,
             }
 
             try:
                 return self.twitter_api.statuses.update_with_media(**params)
             except api.TwitterHTTPError:
-                print('Cannot tweet with media, tweet with media url.')
-                msg['img_url'] = img_url
+                pprint('Cannot tweet with media, tweet with media url.')
+                raise
+            except:
+                traceback.print_exc()
 
+    def tweet_latest_update(self, feed_entry):
+        '''
+        Tweets the latest update, logs when doing so.
+        '''
+
+        msg_limit_length = self.get_msg_limit_length_and_set_urls(feed_entry)
+        self.cram_the_msg(feed_entry, msg_limit_length)
         try:
-            return self.twitter_api.statuses.update(
-                status='\n'.join(filter(bool, msg.values()))
-            )
-        except api.TwitterHTTPError:
-            msg.pop('summary')
-            print(msg_length())
-            print('\n'.join(filter(bool, msg.values())))
-            return self.twitter_api.statuses.update(
-                status='\n'.join(filter(bool, msg.values()))
-            )
+            self.tweet_with_media()
+        except (TypeError, urllib.error.URLError, api.TwitterHTTPError):
+            self.tweet_with_no_media()
+        except:
+            traceback.print_exc()
